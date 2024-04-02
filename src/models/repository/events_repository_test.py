40cb6e4760a40f703c77521832610c6e005bda0d
Rafael Cruz <rcas2009@outlook.com>
2024-04-02 18:34:47 -0300

    improve: Adding try in events_repository

--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -17,8 +17,9 @@ def test_insert_event():
     response = events_repository.insert_event(event)
     print(response)
 
+@pytest.mark.skip(reason="Nao necessita")
 def test_get_event_by_id():
-    event_id = "meu-uuid2"
+    event_id = "meu-uuid2d"
     events_repository = EventsRepository()
     response = events_repository.get_event_by_id(event_id)
     print(response)
