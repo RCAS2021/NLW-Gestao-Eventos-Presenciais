74bea911c738ab37aea426984f0fa4ccec31ec14
Rafael Cruz <rcas2009@outlook.com>
2024-04-02 18:58:50 -0300

    feat: Implementing attendees_repository

---
+++ src/models/repository/attendees_repository_test.py
@@ -0,0 +1,26 @@
+from .attendees_repository import AttendeesRepository
+from src.models.settings.connection import db_connection_handler
+import pytest
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="Novo registro em banco de dados")
+def test_insert_attendee():
+    event_id = "meu-uuid"
+    attendeesInfo = {
+        "uuid": "meu_uuid_attendee",
+        "name": "meu name",
+        "email": "email@email.com",
+        "event_id": event_id
+    }
+    attendees_repository = AttendeesRepository()
+    response = attendees_repository.insert_attendee(attendeesInfo)
+    print(response)
+
+@pytest.mark.skip(reason="nao precisa")
+def test_get_attendee_badge_by_id():
+    attendee_id = "meu_uuid_attendeea"
+    attendees_repository = AttendeesRepository()
+    attendee = attendees_repository.get_attendee_badge_by_id(attendee_id)
+
+    print(attendee)
