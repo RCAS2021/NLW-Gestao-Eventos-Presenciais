8ae93415e18c9619e5bb8697d588c2562c9fb982
Rafael Cruz <rcas2009@outlook.com>
2024-04-02 18:24:43 -0300

    feat: Implementing events_repository

--- src/models/settings/connection.py
+++ src/models/settings/connection.py
@@ -8,18 +8,20 @@ class DBConnectionHandler():
             "storage.db"
         )
         self.__engine = None
-        self.__session = None
+        self.session = None
 
     def connect_to_db(self) -> None:
         self.__engine = create_engine(self.__connection_string)
 
     def get_engine(self):
         return self.__engine
-    
+
     def __enter__(self):
         session_maker = sessionmaker()
-        self.__session = session_maker(bind=self.__engine)
+        self.session = session_maker(bind=self.__engine)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.__session.close()
+        self.session.close()
+
+db_connection_handler = DBConnectionHandler()
